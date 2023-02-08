8ca64d84cd7ef0b5af534eaeac0d79a60027af71
anysa-kharel <anishakharel456@gmail.com>
2023-02-08 15:45:35 +0545

    nav

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import CollapsibleExample from '../components/Nav'
 
 const About = () => {
   return (
     <div>
+      <CollapsibleExample/>
       About Page
     </div>
   )
