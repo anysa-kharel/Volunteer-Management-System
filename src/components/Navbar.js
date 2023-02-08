c5b3ddbf8b18d257d4a5c780b07139bf6ff61546
mandakini <mandakinisapkota@gmail.com>
2023-02-08 15:37:15 +0545

    Added Dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,52 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {BiUser} from 'react-icons/bi';
-import {AiOutlineUserAdd}from 'react-icons/ai'; 
+import { Link } from "react-router-dom";
+import { BiUser } from 'react-icons/bi';
+import { AiOutlineUserAdd } from 'react-icons/ai';
 import './Navbar.css'
 
-const Navbar=()=>{
-    return(
+const Navbar = () => {
+    return (
         <>
-        <div className="main-nav">
+            <div className="main-nav">
 
-            <div className="title">
-                <h2>Volun</h2>
-            </div>
+                <div className="title">
+                    <h2>Volun</h2>
+                </div>
 
-            <div className="Menus">
-                <ul className="nav_item">
+                <div className="Menus">
+                    <ul className="nav_item">
+                        <li>
+                            <Link >
+                                <div className="dropdown">
+                                    <button className="dropbtn no_underline hover_item">Volunteering oppurtunities<i className=""></i></button>
+                                    <div className="dropdown-content">
+                                        <a href="#">Link 1</a>
+                                        <a href="#">Link 2</a>
+                                        <a href="#">Link 3</a>
+                                    </div>
+                                </div>
+                            </Link>
+                    </li>
+
                     <li>
-                    <Link className="no_underline hover_item" > Voluntering Opportunities</Link>
+                        <Link className="no_underline hover_item" to="/about"> About Us</Link>
                     </li>
                     <li>
-                    <Link className="no_underline hover_item" to="/about"> About Us</Link>
-                    </li>
-                    <li>
-                    <Link className="no_underline hover_item" to="/contact"> Contact Us </Link>
+                        <Link className="no_underline hover_item" to="/contact"> Contact Us </Link>
                     </li>
                 </ul>
             </div>
 
             <div className="login">
-                 <ul className="log-in">
+                <ul className="log-in">
                     <li>
-                        <Link className="no_underline hover_item" to="/login">Login <BiUser/></Link>
+                        <Link className="no_underline hover_item" to="/login">Login <BiUser /></Link>
                     </li>
                     <li>
-                        <Link className="no_underline hover_item" to="/signup">Signup <AiOutlineUserAdd/></Link>
+                        <Link className="no_underline hover_item" to="/signup">Signup <AiOutlineUserAdd /></Link>
                     </li>
-                 </ul>
-                 </div>
+                </ul>
+            </div>
         </div>
         </>
     )
